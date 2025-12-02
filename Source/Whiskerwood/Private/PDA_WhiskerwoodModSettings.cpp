7160e12cea678a69784bc76fad01c0e233aac5b9
Zapshark <praedon@yahoo.com>
2025-12-02 05:24:54 -0600

    icon

--- Source/Whiskerwood/Private/PDA_WhiskerwoodModSettings.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "PDA_WhiskerwoodModSettings.h"
-
-FPrimaryAssetId UPDA_WhiskerwoodModSettings::GetPrimaryAssetId() const
-{
-	// Give all these assets a type of "WhiskerwoodMod" so they can be grouped in the Primary Asset system if desired.
-	static const FPrimaryAssetType ModType = TEXT("WhiskerwoodMod");
-
-	return FPrimaryAssetId(ModType, GetFName());
-}
